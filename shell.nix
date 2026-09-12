e29a4744b4d3d8cd522843de640a9d3c0bd957da
graue70 <23035329+graue70@users.noreply.github.com>
2026-09-12 04:13:52 +0200

    update shell.nix for ndk 28 and sdk 36
    Match the new build setup: build tools 36.0.0, platform 36, cmake
    3.31.1, and ndk r28c (28.2.13676358). Point ANDROID_NDK_ROOT and
    ANDROID_NDK at the versioned ndk directory (ndk-bundle no longer
    exists), which build_ffmpeg.sh needs to find the toolchain.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 let
-  buildToolsVersion = "34.0.0";
-  cmakeVersion = "3.22.1";
+  buildToolsVersion = "36.0.0";
+  cmakeVersion = "3.31.1";
+  ndkVersion = "28.2.13676358";
   androidComposition = androidenv.composeAndroidPackages {
     includeEmulator = false;
     includeSources = false;
@@ -10,14 +11,15 @@ let
     useGoogleAPIs = false;
     useGoogleTVAddOns = false;
     includeNDK = true;
-    ndkVersions = ["22.0.7026061"];
-    platformVersions = ["34"];
+    ndkVersions = [ndkVersion];
+    platformVersions = ["36"];
     buildToolsVersions = [buildToolsVersion];
     cmakeVersions = [cmakeVersion];
   };
 in mkShell rec {
   ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+  ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk/${ndkVersion}";
+  ANDROID_NDK = ANDROID_NDK_ROOT;
   GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
   shellHook = ''
     export PATH="$(echo "$ANDROID_SDK_ROOT/cmake/${cmakeVersion}".*/bin):$PATH"
